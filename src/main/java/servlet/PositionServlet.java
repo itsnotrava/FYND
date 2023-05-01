51e49216e6b48e4ff7eda9827d7316c0a85b7549
Nicola Travaglini <nicola1.travaglini@gmail.com>
2023-05-01 21:48:23 +0200

    Implementazione del doPost di PositionServlet.java

--- src/main/java/servlet/PositionServlet.java
+++ src/main/java/servlet/PositionServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import dao.PositionDao;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
 
 @WebServlet(name="PositionServlet", value="/position")
 public class PositionServlet extends HttpServlet {
@@ -16,10 +21,29 @@ public class PositionServlet extends HttpServlet {
 
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.addHeader("Access-Control-Allow-Origin", "*");
 		response.setContentType("application/json");
 
+		String sBody = getBody(request);
+		Gson gson = new Gson();
+		JsonObject jsBody = gson.fromJson(sBody, JsonObject.class);
+
+		String username = jsBody.get("username").getAsString();
+		String position = jsBody.get("position").getAsString();
+
+		try {
+			PositionDao positionDao = new PositionDao();
+			positionDao.insertPosition(username, position);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+		JsonObject jsResponse = new JsonObject();
+		jsResponse.addProperty("result", "success");
+		PrintWriter printWriter = response.getWriter();
+		printWriter.println(jsResponse);
+		printWriter.flush();
 	}
 
 	private static String getBody(HttpServletRequest request) throws IOException {
